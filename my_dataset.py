5fd6bf9846dfee2caf5d8163aa3de4595c061266
Arrebol <1599967289@qq.com>
2023-06-08 16:27:45 +0800

    32

--- my_dataset.py
+++ my_dataset.py
@@ -12,19 +12,22 @@ import os
 #read.readlines()
 
 class VOC2012DataSet(Dataset):
-    def __init__(self,voc_root,transforms,train_set=True):
+    def __init__(self,voc_root,transforms=None, txt_name: str = "train.txt"):
         self.root=os.path.join(voc_root,"VOCdevkit","VOC2012")
         self.img_root=os.path.join(self.root,"JEPGImages")
         self.annotations_root=os.path.join(self.root,"Annotations")
-        if train_set:
-            txt_list=os.path.join(self.root,
-                                  "ImageSets","Main","train.txt")
-        else:
-            txt_list = os.path.join(self.root,
-                                    "ImageSets", "Main", "val.txt")
-        with open(txt_list) as read:
+        txt_path=os.path.join(self.root,"ImageSets","Main",txt_name)
+        # if train_set:
+        #     txt_list=os.path.join(self.root,
+        #                           "ImageSets","Main","train.txt")
+        # else:
+        #     txt_list = os.path.join(self.root,
+        #                             "ImageSets", "Main", "val.txt")
+        with open(txt_path) as read:
             self.xml_list=[os.path.join(self.annotations_root,line.strip()+".xml")
-                           for line in read.readlines()]
+                           for line in read.readlines() if len(line.strip()) > 0]
+
+        self.transforms=transforms
 
         try:
             json_file=open("./pascal_voc_classes.json",'r')
@@ -45,6 +48,7 @@ class VOC2012DataSet(Dataset):
         image = Image.open(img_path)
         if image.format!="JPEG":
             raise ValueError("image'sformat not JPEG")
+
         boxes=[]
         labels=[]
         iscrowd=[]
@@ -58,9 +62,6 @@ class VOC2012DataSet(Dataset):
             iscrowd.append(int(obj["difficult"]))
 
         boxes=torch.as_tensor(boxes,dtype = torch.float32)
-        labels = torch.as_tensor(labels, dtype = torch.int64)
-        iscrowd = torch.as_tensor(iscrowd, dtype = torch.int64)
-        boxes = torch.as_tensor(boxes, dtype = torch.float32)
         labels = torch.as_tensor(labels, dtype = torch.int64)
         iscrowd = torch.as_tensor(iscrowd, dtype = torch.int64)
         image_id = torch.tensor([idx])
@@ -119,6 +120,7 @@ import json
 import matplotlib.pyplot as plt
 import torchvision.transforms as ts
 import random
+import numpy as np
 
 # read class_indict
 category_index = {}
@@ -137,7 +139,7 @@ data_transform = {
 }
 
 # load train data set
-train_data_set = VOC2012DataSet(os.getcwd(), "2012", data_transform["train"], "train.txt")
+train_data_set = VOC2012DataSet(os.getcwd(), transforms =data_transform["train"], txt_name = "train.txt")
 print(len(train_data_set))
 for index in random.sample(range(0, len(train_data_set)), k=5):
     img, target = train_data_set[index]
